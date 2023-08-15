94ef7e4c0692be4a3ea655c5730ebd6ec4b03168
fmrifrey <djfrey@umich.edu>
2023-08-15 11:17:15 -0400

    updated testing script

--- test.c
+++ test.c
@@ -4,45 +4,31 @@
 #include "gradtrap.h"
 #include "helperfuns.h"
 
-#define SLEWMAX 17000
-#define GMAX 4
-
+#define MAX_PG_WAMP 32767
 #define MAXWAVELEN 10000
-#define GRAD_UPDATE_TIME 4
-
-FILE* fID;
+#define GRAD_UPDATE_TIME 4 
+#define XGRAD_max 5
+#define YGRAD_max 5
+#define ZGRAD_max 5
 
 /* cv's */
 int opxres = 64;
 float opfov = 240.0;
-float sp_alpha = 1.0;
+float spalpha = 1.0;
 int ntrains = 1;
 int nnav = 20;
 int sptype2d = 4;
+float SLEWMAX = 17000;
+float GMAX = 4;
+
+/* ipg variables */
+int Gx[MAXWAVELEN], Gy[MAXWAVELEN], Gz[MAXWAVELEN];
+int grad_len;
 
 /* function prototypes */
 int genspiral();
-int gradtrap(float dk, float sm, float gm, float dt, float* h, float* t_ramp, float* t_plat);
-
 int main() {
-/*
-	float tr, tp, h;
-	float dkx, dky, dkz, dk;
-	float sm = SLEWMAX*1e-3;
-	float gm = GMAX;
-	float gam = 4.258*2*M_PI; */ /* gyromagnetic ratio (rad/G/ms) */
-/*
-	dkx = -1.0;
-	dky = -0.75;
-	dkz = 0.5;
-	dk = sqrt(pow(dkx,2) + pow(dky,2) + pow(dkz,2));
-
-	gradtrap(dk, sm, gm, GRAD_UPDATE_TIME*1e-3, &h, &tr, &tp);
-	fprintf(stderr, "h = %f, tr = %f, tp = %f\n", h, tr, tp);
-
-	float A = (tp + tr) / (GRAD_UPDATE_TIME*1e-3) * h;
-	fprintf(stderr, "A = %f\n", A);
-*/
+	
 	genspiral();
 
 	return 1;
@@ -57,29 +43,30 @@ int genspiral() {
 	 *	- kspace Rewinder (*_kr):	kspace rewinder (XYZ trapezoidal gradient)
 	 */
 
-	fID = fopen("ktraj.txt", "w");
+	/* gradient waveforms will be in units of rad*G/ms */
+
+	FILE* fID_ktraj = fopen("ktraj.txt", "w");
+	FILE* fID_grad = fopen("grad.txt", "w");
+	FILE* fID_graddac = fopen("graddac.txt", "w");
 
 	/* declare variables */
 	int n;
 	float t;
 	int np, np_ze, np_sp, np_rd, np_kr;
 	float Tr_ze, Tp_ze, T_sp, T_rd, Tr_kr, Tp_kr;
-	float t1, t2, t3, t4;
+	float t1, t2, t3;
 	float gxn, gyn, gzn, kxn, kyn, kzn;
 	float gx0, gy0, g0, kx0, ky0, kz0, k0;
 	float h_ze, h_kr;
 
 	/* declare waveforms */
-	float gz_ze[MAXWAVELEN] = {0};
 	float kx_sp[MAXWAVELEN], ky_sp[MAXWAVELEN] = {0};
 	float gx_sp[MAXWAVELEN], gy_sp[MAXWAVELEN] = {0};
-	float gx_rd[MAXWAVELEN], gy_rd[MAXWAVELEN] = {0};
-	float gx_kr[MAXWAVELEN], gy_kr[MAXWAVELEN], gz_kr[MAXWAVELEN] = {0};
 	float gx[MAXWAVELEN], gy[MAXWAVELEN], gz[MAXWAVELEN] = {0};
 
 	/* convert units */
 	float dt = GRAD_UPDATE_TIME*1e-3; /* raster time (ms) */
-	float D = (float)opfov / 10; /* fov (cm) */
+	float D = (float)opfov / 10.0; /* fov (cm) */
 	float gm = GMAX; /* gradient amplitude limit (G/cm) */
 	float sm = SLEWMAX * 1e-3; /* slew limit (G/cm/ms) */
 	float gam = 4.258*2*M_PI; /* gyromagnetic ratio (rad/G/ms) */
@@ -90,7 +77,7 @@ int genspiral() {
 	np_ze = round((2*Tr_ze + Tp_ze) / dt);
 
 	/* generate the spiral trajectory */
-	T_sp = kimvdsp(kx_sp, ky_sp, D, sm, gm, opxres, (sptype2d < 3) ? (ntrains) : (2*ntrains), sp_alpha, dt);
+	T_sp = kimvdsp(kx_sp, ky_sp, D, sm, gm, opxres, (sptype2d < 3) ? (ntrains) : (2*ntrains), spalpha, dt);
 	np_sp = round(T_sp/dt);
 	diff(kx_sp, np_sp, gam*dt, gx_sp);
 	diff(ky_sp, np_sp, gam*dt, gy_sp);	
@@ -110,7 +97,6 @@ int genspiral() {
 	ky0 = ky_sp[np_sp - 2] + gam * 1/2 * (T_rd + dt) * gy0;
 	kz0 = kmax;
 	k0 = sqrt(pow(kx0,2) + pow(ky0,2) + pow(kz0,2));
-	fprintf(stderr, "k0 = [%f, %f, %f]\n", kx0,ky0,kz0);
 
 	/* generate the kspace rewinder */
 	gradtrap(k0, sm, gm, dt, &h_kr, &Tr_kr, &Tp_kr); 
@@ -120,7 +106,6 @@ int genspiral() {
 	t1 = 2*Tr_ze + Tp_ze;
 	t2 = t1 + T_sp;
 	t3 = t2 + T_rd;
-	t4 = t3 + 2*Tr_kr + Tp_kr;
 
 	/* calculate total number of points */
 	np = np_ze + np_sp + np_rd + np_kr + 1;
@@ -181,26 +166,39 @@ int genspiral() {
 				gy[np + nnav + n] = gyn;
 				gz[np + nnav + n] = gzn;
 				break;
+			default:
+				return 0;
+				break;
 		}
 	}
 
-	/* correct the number of points */
+	/* calculate total number of points */
 	if (sptype2d > 2)
-		np = 2*np + nnav;
+		grad_len = 2*np + nnav;
+	else
+		grad_len = np;
 
-	/* calculate kspace location and write to file */
-	for (n = 0; n < np; n++) {
+	/* calculate kspace location, fs gradients, and write to file */
+	kxn = 0.0;
+	kyn = 0.0;
+	kzn = 0.0;
+	for (n = 0; n < grad_len; n++) {
 		kxn += gam * gx[n] * dt;
 		kyn += gam * gy[n] * dt;
 		kzn += gam * gz[n] * dt;
-		fprintf(fID, "%f \t%f \t%f\n", kxn, kyn, kzn);
-		if (n == np_sp + np_ze - 1)
-			fprintf(stderr, "at pt %d: kx = %f, ky = %f, kz = %f\n", n,kxn,kyn,kzn);
-		if (n == np_sp + np_ze + np_rd - 1)
-			fprintf(stderr, "at pt %d: kx = %f, ky = %f, kz = %f\n", n,kxn,kyn,kzn);
+
+		Gx[n] = 2*round(MAX_PG_WAMP/XGRAD_max * gx[n]/(2.0*M_PI) / 2.0);
+		Gy[n] = 2*round(MAX_PG_WAMP/YGRAD_max * gy[n]/(2.0*M_PI) / 2.0);
+		Gz[n] = 2*round(MAX_PG_WAMP/ZGRAD_max * gz[n]/(2.0*M_PI) / 2.0);
+		
+		fprintf(fID_ktraj, "%f \t%f \t%f\n", kxn, kyn, kzn);
+		fprintf(fID_grad, "%f \t%f \t%f\n", gx[n], gy[n], gz[n]);
+		fprintf(fID_graddac, "%d \t%d \t%d\n", Gx[n], Gy[n], Gz[n]);
 	}
 
-	fclose(fID);
+	fclose(fID_ktraj);
+	fclose(fID_grad);
+	fclose(fID_graddac);
 
 	return 1;
 }
